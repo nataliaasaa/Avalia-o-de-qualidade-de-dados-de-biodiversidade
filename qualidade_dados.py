8a1602ad31727ec2ea7fbc923c934f935cc14067
nataliaasaa <nataliaasaa@gmail.com>
2020-08-27 16:22:56 -0300

    lendo coluna

--- qualidade_dados.py
+++ qualidade_dados.py
@@ -1,7 +1,22 @@
 class QualidadeDados:
 
-    def __init__(self)
-        self.file = open("portalbio_export_27-08-2020-14-01-51.cvs", "r")
+    def __init__(self):
+        self.file = open("portalbio_export_27-08-2020-14-01-51.csv", "r")
+        self.data = None
+        self.file.close()
 
-    def nome_funcao()
-        pass
+    def listdata(self):
+        self.file = open("portalbio_export_27-08-2020-14-01-51.csv", "r")
+        self.data = [[campo for campo in linha.split(';')] for linha in self.file.read().split('\n')[1:-1]]
+        #print (self.data)
+        self.file.close()
+
+    def nivelTaxonomico(self):
+        if self.data is None:
+            self.listdata()
+        coluna1 = [str(linha[15]) for linha in self.data]
+        print (coluna1)
+
+obj = QualidadeDados()
+obj.listdata()
+obj.nivelTaxonomico()
