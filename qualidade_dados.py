dbea7119f0e611fa2832d955426162d84aafc1c3
nataliaasaa <nataliaasaa@gmail.com>
2020-08-28 09:52:25 -0300

    verifica nivel taxonomico

--- qualidade_dados.py
+++ qualidade_dados.py
@@ -3,20 +3,25 @@ class QualidadeDados:
     def __init__(self):
         self.file = open("portalbio_export_27-08-2020-14-01-51.csv", "r")
         self.data = None
-        self.file.close()
 
     def listdata(self):
-        self.file = open("portalbio_export_27-08-2020-14-01-51.csv", "r")
         self.data = [[campo for campo in linha.split(';')] for linha in self.file.read().split('\n')[1:-1]]
         #print (self.data)
         self.file.close()
 
-    def nivelTaxonomico(self):
+    def nivelTaxonomico(self): #verifica até qual nivel taxonomico a ocorrência foi identificada (0 a 6) 
         if self.data is None:
             self.listdata()
-        coluna1 = [str(linha[15]) for linha in self.data]
-        print (coluna1)
 
+        for linha in self.data:
+            count = 0
+            for i in linha[15:21]:
+                if i == 'Sem Informações':
+                    count = 0
+                else:
+                    count += 1
+            print ("Nivel taxonômico da ocorrência : ", count)
+             
 obj = QualidadeDados()
 obj.listdata()
 obj.nivelTaxonomico()
