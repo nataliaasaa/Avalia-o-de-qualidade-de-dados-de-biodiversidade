69c6fb25ffa6aed363dc894b81153e0ea8c13297
PedroNunes92 <pedro.nunes@sistemafiep.org.br>
2020-08-28 11:47:54 -0300

    filtros terminados

--- qualidade_dados.py
+++ qualidade_dados.py
@@ -17,19 +17,26 @@ class QualidadeDados:
         coluna1 = [str(linha[15]) for linha in self.data]
         print (coluna1)
    
-    def filtros(self):
+    def filtros(self): #CRIAR UM DICIONARIO COM A SIGLA E O NUMERO DE OCORRENCIA E AI ENVONTRAR O NUMERO DIGITANDO A SIGLA
         if self.data is None:
             self.listdata()
-        coluna3 = [str(linha[26]) for linha in self.data]
-        #CRIAR UM DICIONARIO COM A SIGLA E O NUMERO DE OCORRENCIA E AI ENVONTRAR O NUMERO DIGITANDO A SIGLA
-        #estado = input("Digite a sigla do estado em maiúsculo: ")
-        ocor = [coluna3.count(a) for a in coluna3]
-        dic_ocor = dict(zip(coluna3, ocor))
-        #print (coluna3)
-        sigla = input("Digite uma sigla maiúscula: ")
-        print(dic_ocor)
-        print(dic_ocor[sigla])         
-
+        #FILTRO ESTADOS
+        estados = [str(linha[26]) for linha in self.data] #coluna dos estados   
+        ocor = [estados.count(a) for a in estados] #coluna da ocorrencia
+        dic_ocor = dict(zip(estados, ocor)) #dicionario 
+        sigla = input("Digite uma sigla maiúscula: ") #ver questao da letra maiuscula e minuscula
+        sigla = sigla.upper()
+        print(dic_ocor[sigla]) 
+        #FILTRO ESPECIE - CATEGORA de AMEAÇA
+        especie = [str(linha[21]) for linha in self.data] #coluna das especies trans em linha
+        especie = especie[0]
+        categoria = [str(linha[23]) for linha in self.data] #coluna das especies trans em linha
+        categoria = categoria[0]
+        nome = input("Digite especie: ")
+        if nome in especie:
+            print(categoria) 
+        else:
+            print("Nome da especie esta escrita errada")
 obj = QualidadeDados()
 obj.listdata()
 #obj.nivelTaxonomico()
