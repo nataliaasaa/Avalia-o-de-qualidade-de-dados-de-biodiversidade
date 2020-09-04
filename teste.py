11860747846409fb5ca37950f90673d9b53fe160
nataliaasaa <nataliaasaa@gmail.com>
2020-09-04 15:54:11 -0300

    Uncomment teste

--- teste.py
+++ teste.py
@@ -2,26 +2,26 @@
 #Primeiro passo: pip install -i https://test.pypi.org/simple/ qualidadedados
 #pip install opencage (Pacote utilizado na classe)
 
-from qualidadedados import qualidade_dados
 #from nome do pacote import nome do arquivo
+from qualidadedados import qualidade_dados
 
 #Nome_do_arquivo.classe().função()
 
 #Mostra a média de dados Sem informação por coluna
-#qualidade_dados.QualidadeDados().dadosFaltantesPorColuna() 
+qualidade_dados.QualidadeDados().dadosFaltantesPorColuna() 
 
 #Mostra o nivelTaxonomico da ocorrencia
-#qualidade_dados.QualidadeDados().nivelTaxonomico()
+qualidade_dados.QualidadeDados().nivelTaxonomico()
 
 #Recebe um estado (sigla) e retorna o número de indivíduos do arquivo registrados no estado indicado
-#qualidade_dados.QualidadeDados().filtros_estados()
+qualidade_dados.QualidadeDados().filtros_estados()
 
 #Recebe uma especie e retorna o nivel de ameaça
-#qualidade_dados.QualidadeDados().filtros_especie()
+qualidade_dados.QualidadeDados().filtros_especie()
 
 #Retorna o numero total de indivíduos em um arquivo
 qualidade_dados.QualidadeDados().numTotalIndividuos()
 
 #Verifica se as coordenadas indicadas correspondem ao estado indicado
-#qualidade_dados.QualidadeDados().verificarCoordenadas()
+qualidade_dados.QualidadeDados().verificarCoordenadas()
 
